879aa851f21edbcbb87137110754911477dd266e
Colin Dembovsky <colindembovsky@gmail.com>
2019-12-19 12:54:52 +0200

    Rename env var

--- automl/scripts/get_data.py
+++ automl/scripts/get_data.py
@@ -5,8 +5,8 @@ import pandas as pd
 
 def get_data():
     print("In get_data")
-    print(os.environ['AZUREML_DATAREFERENCE_output_split_train_x'])
-    X_train = pd.read_csv(os.environ['AZUREML_DATAREFERENCE_output_split_train_x'] + "/data", header=0)
-    y_train = pd.read_csv(os.environ['AZUREML_DATAREFERENCE_output_split_train_y'] + "/data", header=0)
+    print(os.environ['AZUREML_DATAREFERENCE_diabetes_automl_split_train_x'])
+    X_train = pd.read_csv(os.environ['AZUREML_DATAREFERENCE_diabetes_automl_split_train_x'] + "/data", header=0)
+    y_train = pd.read_csv(os.environ['AZUREML_DATAREFERENCE_diabetes_automl_split_train_y'] + "/data", header=0)
 
     return {"X": X_train.values, "y": y_train.values.flatten()}
