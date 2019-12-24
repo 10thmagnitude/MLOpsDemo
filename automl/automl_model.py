462b7beb73993dfa741530a89a57836420c1de2c
Colin Dembovsky <colindembovsky@gmail.com>
2019-12-24 22:49:53 +0200

    Fix deps

--- automl/automl_model.py
+++ automl/automl_model.py
@@ -75,10 +75,9 @@ aml_run_config.environment.docker.enabled = True
 aml_run_config.environment.docker.base_image = "mcr.microsoft.com/azureml/base:intelmpi2018.3-ubuntu16.04"
 aml_run_config.environment.python.user_managed_dependencies = False
 aml_run_config.environment.python.conda_dependencies = CondaDependencies.create(
-    python_version="3.6.9",
-    conda_packages=['pandas==0.23.4', 'scikit-learn==0.20.3'], 
-    pip_packages=['azureml-sdk==1.0.76', 'azureml-dataprep==1.1.33', 'azureml-dataprep[pandas]', 'azureml-train-automl==1.0.76', 'numpy==1.16.2'], 
-    pin_sdk_version=False)
+    conda_packages=['numpy', 'pandas', 'scikit-learn'], 
+    pip_packages=['azureml-sdk', 'azureml-core', 'azureml-dataprep', 'azureml-dataprep[pandas]', 'azureml-train-automl']
+)
 
 scripts_folder = './scripts'
 prepared_data = PipelineData("diabetes_data_prep", datastore=datastore)
